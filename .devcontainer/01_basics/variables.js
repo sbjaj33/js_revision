f1b63da4c57cc77bdf758b4717aaaa4ae642c30f
Shivam Bajaj <52606915+sbjaj33@users.noreply.github.com>
2025-08-17 17:30:12 +0000

    new file

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+let accountNumber = 1234
+const accountId = 342
+var accountName = 'Shivam Bajaj'
+
+/*
+don't use var as due to block & functional scope issue
+*/
+console.log('output display')
+console.table([accountNumber,accountId,accountName])
